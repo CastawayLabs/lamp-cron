a5a0be983f9bfcaf8fadce3f53c73ed7fa288d6d
Matej Kramny <git@matej.me>
2014-09-01 19:49:28 +0100

    Create lamp.sh

---
+++ conf/lamp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cp /etc/resolv.conf /var/spool/postfix/etc/
+
+if [ ! -z "$MANDRILL_PWD" ]
+then
+  echo "Using Mandrill Password. <= Written to /etc/postfix/sasl_passwd"
+
+  echo "[smtp.mandrillapp.com]:587 $MANDRILL_PWD" > /etc/postfix/sasl_passwd
+  chown -R postfix:postfix /etc/postfix
+  chmod 600 /etc/postfix/sasl_passwd
+  postmap /etc/postfix/sasl_passwd
+fi
+
+if [ ! -z "$RSYSLOG" ]
+then
+  echo "Adding RSYSLOGD configuration"
+  
+  echo "$RSYSLOG" >> /etc/rsyslog.conf
+fi
+
+# Load the crontab
+crontab /etc/crontab.txt
+
+/usr/bin/supervisord
